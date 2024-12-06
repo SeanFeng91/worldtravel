f3c32a93151d8e96b30e985a627a170da54e1519
SeanFeng91 <fengyx91@gmail.com>
2024-12-06 10:52:43 +0800

    换成minisearch

--- .vitepress/cache/deps/vitepress___@vue_devtools-api.js
+++ .vitepress/cache/deps/vitepress___@vue_devtools-api.js
@@ -1,4 +1,4 @@
-import "./chunk-L6OFPWCY.js";
+import "./chunk-HKJ2B2AA.js";
 
 // node_modules/@vue/devtools-shared/dist/index.js
 var __create = Object.create;
--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -108,52 +108,56 @@ export default defineConfig({
       copyright: 'Copyright © 2024-present WorldTravel'
     },
     
+    // search: {
+    //   provider: 'algolia',
+    //   options: {
+    //     appId: '52C6J0MS4V',
+    //     apiKey: '816dc34de3e9c7f6b2d4f95bb85ba4a5',
+    //     indexName: 'worldtravel',
+    //     placeholder: '搜索文档',
+    //     translations: {
+    //       button: {
+    //         buttonText: '搜索',
+    //         buttonAriaLabel: '搜索'
+    //       },
+    //       modal: {
+    //         searchBox: {
+    //           resetButtonTitle: '清除查询条件',
+    //           resetButtonAriaLabel: '清除查询条件',
+    //           cancelButtonText: '取消',
+    //           cancelButtonAriaLabel: '取消'
+    //         },
+    //         startScreen: {
+    //           recentSearchesTitle: '搜索历史',
+    //           noRecentSearchesText: '没有搜索历史',
+    //           saveRecentSearchButtonTitle: '保存至搜索历史',
+    //           removeRecentSearchButtonTitle: '从搜索历史中移除',
+    //           favoriteSearchesTitle: '收藏',
+    //           removeFavoriteSearchButtonTitle: '从收藏中移除'
+    //         },
+    //         errorScreen: {
+    //           titleText: '无法获取结果',
+    //           helpText: '你可能需要检查你的网络连接'
+    //         },
+    //         footer: {
+    //           selectText: '选择',
+    //           navigateText: '切换',
+    //           closeText: '关闭',
+    //           searchByText: '搜索提供者'
+    //         },
+    //         noResultsScreen: {
+    //           noResultsText: '无法找到相关结果',
+    //           suggestedQueryText: '你可以尝试查询',
+    //           reportMissingResultsText: '你认为该查询应该有结果？',
+    //           reportMissingResultsLinkText: '点击反馈'
+    //         }
+    //       }
+    //     }
+    //   }
+    // }
     search: {
-      provider: 'algolia',
-      options: {
-        appId: '52C6J0MS4V',
-        apiKey: '816dc34de3e9c7f6b2d4f95bb85ba4a5',
-        indexName: 'worldtravel',
-        placeholder: '搜索文档',
-        translations: {
-          button: {
-            buttonText: '搜索',
-            buttonAriaLabel: '搜索'
-          },
-          modal: {
-            searchBox: {
-              resetButtonTitle: '清除查询条件',
-              resetButtonAriaLabel: '清除查询条件',
-              cancelButtonText: '取消',
-              cancelButtonAriaLabel: '取消'
-            },
-            startScreen: {
-              recentSearchesTitle: '搜索历史',
-              noRecentSearchesText: '没有搜索历史',
-              saveRecentSearchButtonTitle: '保存至搜索历史',
-              removeRecentSearchButtonTitle: '从搜索历史中移除',
-              favoriteSearchesTitle: '收藏',
-              removeFavoriteSearchButtonTitle: '从收藏中移除'
-            },
-            errorScreen: {
-              titleText: '无法获取结果',
-              helpText: '你可能需要检查你的网络连接'
-            },
-            footer: {
-              selectText: '选择',
-              navigateText: '切换',
-              closeText: '关闭',
-              searchByText: '搜索提供者'
-            },
-            noResultsScreen: {
-              noResultsText: '无法找到相关结果',
-              suggestedQueryText: '你可以尝试查询',
-              reportMissingResultsText: '你认为该查询应该有结果？',
-              reportMissingResultsLinkText: '点击反馈'
-            }
-          }
-        }
-      }
+      provider: 'local',
+      
     }
   },
   
